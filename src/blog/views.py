4542eea27910130b92e7654f1c94e61cdcb875ef
furkan-cloud <firstinfirstout2020@gmail.com>
2021-01-05 23:00:18 +0300

    update and delete feat added

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
 
 def post_list(request):
-    qs = Post.objects.all()
+    qs = Post.objects.filter(status="p")
     context = {
         "object_list": qs
     }
@@ -26,3 +26,34 @@ def post_create(request):
         'form': form
     }
     return render(request, "blog/post_create.html", context)
+
+def post_detail(request, slug):
+    # Post.objects.get(slug=learn-css-086ca595a4)
+    obj = get_object_or_404(Post, slug=slug)
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_detail.html", context)
+
+def post_update(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect("blog:list")
+
+    context = {
+        "object": obj,
+        "form": form
+    }
+    return render(request, "blog/post_update.html", context)
+
+def post_delete(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    if request.method == "POST":
+        obj.delete()
+        return redirect("blog:list")
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_delete.html", context)
